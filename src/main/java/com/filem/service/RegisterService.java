a834e7526c6d80e906fb8377cf4adcd3f758bf77
Your Name <you@example.com>
2019-10-22 23:16:35 +0500

    add DIRECTORY for new user

--- src/main/java/com/filem/service/RegisterService.java
+++ src/main/java/com/filem/service/RegisterService.java
@@ -3,18 +3,25 @@ package com.filem.service;
 import com.filem.accounts.UserProfile;
 import com.filem.db.UserProfileDAOImpl;
 
+import java.io.File;
+
 public class RegisterService {
     public UserProfile Register(String login, String pass, String mail) {
         UserProfileDAOImpl base = new UserProfileDAOImpl();
         if (!base.containsLogin(login)) {
 
 
-                base.addUser(login, pass, mail);
-                System.out.println(login);
-                return new UserProfile(login, pass, mail);
+            base.addUser(login, pass, mail);
+            System.out.println(login);
+            File folder = new File("D:\\" + login);
+            if (!folder.exists()) {
+                folder.mkdirs();
+
+            }
+            return new UserProfile(login, pass, mail);
 
         } else {
-            System.out.println("incorrect login");;
+            System.out.println("incorrect login");
         }
         return null;
     }
